6e75ab4d77f636788d45afcd30e619777c69293e
otis-tran <otis.tran2000@gmail.com>
2025-09-01 21:19:52 +0700

    Add Prettier to format project

--- src/app/wagmi.config.ts
+++ src/app/wagmi.config.ts
@@ -1,16 +1,18 @@
-import { createConfig, http  } from 'wagmi';
+import { createConfig, http } from 'wagmi';
 import { sepolia } from 'wagmi/chains';
-import { injected, walletConnect, metaMask, safe } from 'wagmi/connectors'
+import { injected, walletConnect, metaMask, safe } from 'wagmi/connectors';
 
 export const config = createConfig({
   chains: [sepolia],
   connectors: [
     injected(),
-    walletConnect({ projectId: process.env.WALLETCONNECT_PROJECT_ID as string }),
+    walletConnect({
+      projectId: process.env.WALLETCONNECT_PROJECT_ID as string,
+    }),
     metaMask(),
     safe(),
   ],
   transports: {
-    [sepolia.id]: http()
+    [sepolia.id]: http(),
   },
-})
+});
